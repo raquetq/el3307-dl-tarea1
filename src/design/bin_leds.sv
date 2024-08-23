1a81ff2422ea12fa835b8aad20545b09b6b6a429
Dennis Arce <dennisarce99@estudiantec.cr>
2024-08-22 20:11:44 -0600

    Subsistema leds bin v1

---
+++ src/design/bin_leds.sv
@@ -0,0 +1,11 @@
+module moduleLED(
+    input logic [3:0] cod_bin,
+    output logic [3:0] codigo_bin_led_po  
+);
+
+    assign codigo_bin_led_po[0] = cod_bin[0] ? 1:0;
+    assign codigo_bin_led_po[1] = cod_bin[1] ? 1:0;
+    assign codigo_bin_led_po[2] = cod_bin[2] ? 1:0;
+    assign codigo_bin_led_po[3] = cod_bin[3] ? 1:0;
+
+endmodule
