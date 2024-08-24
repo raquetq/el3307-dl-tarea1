0043c3f37f186e3040129a6eedffca06d8cfd78f
Dennis Arce <dennisarce99@estudiantec.cr>
2024-08-24 17:56:08 -0600

    7seg v3 + Act. Inf

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## 1. Abreviaturas y definiciones
 - **FPGA**: Field Programmable Gate Arrays
+- **MSB**: Bit más significativo
 
 ## 2. Referencias
 [0] David Harris y Sarah Harris. *Digital Design and Computer Architecture. RISC-V Edition.* Morgan Kaufmann, 2022. ISBN: 978-0-12-820064-3
@@ -13,26 +14,23 @@
 ### 3.1 Módulo 1 (Código Gray a Código Binario)
 #### 1. Encabezado del módulo
 ```SystemVerilog
-module mi_modulo(
-    input logic     entrada_i,      
-    output logic    salida_i 
-    );
+module moduleGray (
+    input logic [3:0] codigo_gray_pi,
+    output logic [3:0] cod_bin
+);
 ```
 #### 2. Parámetros
 - Lista de parámetros
 
 #### 3. Entradas y salidas:
-- `entrada_i`: descripción de la entrada
-- `salida_o`: descripción de la salida
+- `codigo_gray_pi`: Entrada de 4 bits para recibir el código Gray 
+- `cod_bin`: Salida de 4 bits con valor binario del código Gray ingresado
 
 #### 4. Criterios de diseño
-Diagramas, texto explicativo...
+Basándose de la conversión de código Gray a código binario se buscó una forma de representarlo en ecuaciones lógicas. Para convertir un número en código Gray a código binario, primero, se mantiene el MSB, y luego, se toma este valor y se suma al siguiente bit, si la suma da como resultado 10'b, se descarta el acarreo, y a partir de este punto puede ser visto como una operación XOR entre el resultado y el siguiente bit.
 
 #### 5. Testbench
-Descripción y resultados de las pruebas hechas
-
-### Otros modulos
-- agregar informacion siguiendo el ejemplo anterior.
+Para el testbench de este módulo se demostró que la conversión funcionara de acuerdo con la tabla de verdad realizada para todo valor de la misma visualizando el resultado la consola de comandos. Cada dato ingresado lanzó el resultado correcto.
 
 ### 3.2 Módulo 2 (Código Binario en LEDs)
 #### 1. Encabezado del módulo
--- src/design/7seg.sv
+++ src/design/7seg.sv
@@ -4,34 +4,32 @@ module module7SEG(
     output logic [6:0] catodo_po
 );
 
+    //Codigo binario en Conmutadores
     assign A = cod_bin[3];
     assign B = cod_bin[2];
     assign C = cod_bin[1];
     assign D = cod_bin[0];
 
-    //seg a
-    assign seg_a = (B || D) && (!A || C) && (A || !C) && (!B || !D); //seg a unidades
-    
-    //seg b
-    assign seg_b = (!C || !D) && (A || !C) && (A || !D) && (!B) && (!A || C || D); //seg b unidades
+    //7seg unidades
+    assign seg_a = (B || D) && (!A || C) && (A || !C) && (!B || !D);
+    assign seg_b = (!C || !D) && (A || !C) && (A || !D) && (!B) && (!A || C || D);
+    assign seg_c = (A || !B) && (B || C) && (!A || !C) && (D);
+    assign seg_d = (A || !C) && (!B || !D) && (A || B || D) && (!A || !B || C) && (B || !C || D) && (!A || C || !D);
+    assign seg_e = (A || !C || !D) && (!A || C || !D) && (!B || !D);
+    assign seg_f = (!A || !C || !D) && (B || C || !D) && (B) && (A || !B || !D) && (!B || !C || !D);
+    assign seg_g = (B || !C) && (A || B) && (A || !C) && (!A || C ||!D) && (!A || !B || C) ;
 
-    //seg c
-    assign seg_c = (A || !B) && (B || C) && (!A || !C) && (D); //seg c unidades
+    //7SEG DECENAS
+    assign sd_X = (!B) && (!A || !C);
+    assign sd_Y = 1'b1;
 
-    //seg d
-    assign seg_d = (A || !C) && (!B || !D) && (A || B || D) && (!A || !B || C) && (B || !C || D) && (!A || C || !D); //seg d unidades
-
-    //seg e
-    assign seg_e = (A || !C || !D) && (!A || C || !D) && (!B || !D); //seg e unidades
-
-    //seg f
-    assign seg_f = (!A || !C || !D) && (B || C || !D) && (B) && (A || !B || !D) && (!B || !C || !D) ; //seg f unidades
-
-    //seg g
-    assign seg_g = (B || !C) && (A || B) && (A || !C) && (!A || C ||!D) && (!A || !B || C) ; //seg g unidades
-
-    //SEG DECENAS
-    assign seg_dec_X = (!B) && (!A || !C);
-    assign seg_dec_Y = 1'b1;
+    //Salida
+    assign catodo_po[0] = ((!anodo_po[1] || anodo_po[0] || !sd_X || seg_a)&&(anodo_po[1] || anodo_po[0] || sd_X || !seg_a)) ? [seg_a]:[sd_X];
+    assign catodo_po[1] = ((!anodo_po[1] || anodo_po[0] || !sd_Y || seg_b)&&(anodo_po[1] || anodo_po[0] || sd_Y || !seg_b)) ? [seg_b]:[sd_Y];
+    assign catodo_po[2] = ((!anodo_po[1] || anodo_po[0] || !sd_Y || seg_c)&&(anodo_po[1] || anodo_po[0] || sd_Y || !seg_c)) ? [seg_c]:[sd_Y];
+    assign catodo_po[3] = ((!anodo_po[1] || anodo_po[0] || !sd_X || seg_d)&&(anodo_po[1] || anodo_po[0] || sd_X || !seg_d)) ? [seg_d]:[sd_X];
+    assign catodo_po[4] = ((!anodo_po[1] || anodo_po[0] || !sd_X || seg_e)&&(anodo_po[1] || anodo_po[0] || sd_X || !seg_e)) ? [seg_e]:[sd_X];
+    assign catodo_po[5] = ((!anodo_po[1] || anodo_po[0] || !sd_X || seg_f)&&(anodo_po[1] || anodo_po[0] || sd_X || !seg_f)) ? [seg_f]:[sd_X];
+    assign catodo_po[6] = ((!anodo_po[1] || anodo_po[0] || !1'b0 || seg_g)&&(anodo_po[1] || anodo_po[0] || 1'b0 || !seg_g)) ? [seg_g]:[1'b0];
 
 endmodule
