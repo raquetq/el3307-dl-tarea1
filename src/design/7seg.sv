e8c851ec4f2acb2650c90e9b76324c36943a49cd
Dennis Arce <dennisarce99@estudiantec.cr>
2024-08-24 13:11:53 -0600

    Subsistema leds bin v2

---
+++ src/design/7seg.sv
@@ -0,0 +1,22 @@
+module module7SEG(
+    input logic [3:0] cod_bin;
+    output logic [1:0] anodo_po;
+    output logic [6:0] catodo_po;
+)
+
+    assign A = cod_bin[3];
+    assign B = cod_bin[2];
+    assign C = cod_bin[1];
+    assign D = cod_bin[0];
+
+    //seg a
+    assign seg_a = ;
+    
+    //seg b
+    //seg c
+    //seg d
+    //seg e
+    //seg f
+    //seg g
+
+endmodule
--- src/design/bin_leds.sv
+++ src/design/bin_leds.sv
@@ -3,9 +3,9 @@ module moduleLED(
     output logic [3:0] codigo_bin_led_po  
 );
 
-    assign codigo_bin_led_po[0] = cod_bin[0] ? 1:0;
-    assign codigo_bin_led_po[1] = cod_bin[1] ? 1:0;
-    assign codigo_bin_led_po[2] = cod_bin[2] ? 1:0;
-    assign codigo_bin_led_po[3] = cod_bin[3] ? 1:0;
+    assign codigo_bin_led_po[0] = (cod_bin[0] == 1'b1) ? 1:0;
+    assign codigo_bin_led_po[1] = (cod_bin[1] == 1'b1) ? 1:0;
+    assign codigo_bin_led_po[2] = (cod_bin[2] == 1'b1) ? 1:0;
+    assign codigo_bin_led_po[3] = (cod_bin[3] == 1'b1) ? 1:0;
 
 endmodule
