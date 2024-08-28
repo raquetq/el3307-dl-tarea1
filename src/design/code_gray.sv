99cf558d54006bb1b0a07cf1e26a33d3e0d8f135
Dennis Arce <dennisarce99@estudiantec.cr>
2024-08-28 17:37:20 -0600

    prueba

--- src/design/code_gray.sv
+++ src/design/code_gray.sv
@@ -3,9 +3,9 @@ module moduleGray (
     output logic [3:0] cod_bin
 );
     
-    assign cod_bin[3] = codigo_gray_pi[3];
-    assign cod_bin[2] = cod_bin[3] ^ codigo_gray_pi[2];
-    assign cod_bin[1] = cod_bin[2] ^ codigo_gray_pi[1];
-    assign cod_bin[0] = cod_bin[1] ^ codigo_gray_pi[0];
+    assign cod_bin[3] = (codigo_gray_pi[3]) ? 1'b1:1'b0;
+    assign cod_bin[2] = (cod_bin[3] ^ codigo_gray_pi[2]) ? 1'b1:1'b0;
+    assign cod_bin[1] = (cod_bin[2] ^ codigo_gray_pi[1]) ? 1'b1:1'b0;
+    assign cod_bin[0] = (cod_bin[1] ^ codigo_gray_pi[0]) ? 1'b1:1'b0;
 
 endmodule
