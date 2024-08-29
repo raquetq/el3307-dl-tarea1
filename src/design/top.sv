20059abafbfba67bbdb49f9fedd657d63d2d54b1
DJosueMM <djosuemm@gmail.com>
2024-08-29 17:39:38 -0600

    error del bitstream

--- src/design/top.sv
+++ src/design/top.sv
@@ -1,7 +1,7 @@
 module TopModule (
     input logic [3:0] codigo_gray_pi,
     input logic rst_pi,
-    output logic [3:0] cod_bin,
+    //output logic [3:0] cod_bin,
     output logic [3:0] codigo_bin_led_po,
     output logic [1:0] anodo_po,
     output logic [6:0] catodo_po
